b1febfe87ae16518b5bda02669f5661d1ae92641
Steph3n-T <stamasi@498robotics.org>
2024-09-20 19:07:37 -0700

    shooter util updates

--- src/main/java/org/team498/C2024/ShooterUtil.java
+++ src/main/java/org/team498/C2024/ShooterUtil.java
@@ -4,7 +4,8 @@ import org.team498.lib.util.LinearInterpolator;
 
 public class ShooterUtil {
     private static double[][] data = new double[][] {
-        {3.8, 28},
+        {4.0, 26},
+        {3.8, 26.5},
         {3.5, 30},
         {3.4, 3.5},
         {3.3, 31.5},
@@ -13,6 +14,7 @@ public class ShooterUtil {
         {2.6, 37},
         {2.5, 38},
         {2.4, 39},
+        {2.3, 40},
         {2.2, 41},
         {2.1, 42},
         {2.0, 44},
@@ -20,8 +22,8 @@ public class ShooterUtil {
         {1.3, 55},
     };
     private static double[][] speedData = new double[][] {
-        {0, 4500},
-        {3.7, 4500},
+        {0, 3500},
+        {3.7, 3800},
         {4.0, 4500}
     };
     private static LinearInterpolator interpolator = new LinearInterpolator(data);
