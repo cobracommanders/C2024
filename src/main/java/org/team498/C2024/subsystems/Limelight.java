10a0778b849fde079bb7867c11352b4588b2edd7
Bella <iyates@498robotics.org>
2024-12-10 19:01:35 -0700

    12/10/2024 limelight localization code

--- src/main/java/org/team498/C2024/subsystems/Limelight.java
+++ src/main/java/org/team498/C2024/subsystems/Limelight.java
@@ -1,10 +1,13 @@
 package org.team498.C2024.subsystems;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.estimator.PoseEstimator;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import org.team498.lib.LimelightHelpers;
 import org.team498.lib.LimelightHelpers.LimelightTarget_Detector;
+import org.team498.lib.LimelightHelpers.PoseEstimate;
 
 
 public class Limelight extends SubsystemBase{
@@ -36,6 +39,28 @@ public class Limelight extends SubsystemBase{
         return false;
         return true;
     }
+
+    int[] validIDs = {3,4};
+        LimelightHelpers.SetFiducialIDFiltersOverride("limelight", validIDs);
+
+      LimelightHelpers.SetRobotOrientation("limelight", m_poseEstimator.getEstimatedPosition().getRotation().getDegrees(), 0, 0, 0, 0, 0);
+      LimelightHelpers.PoseEstimate mt2 = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("limelight");
+      if(Math.abs(m_gyro.getRate()) > 720) // if our angular velocity is greater than 720 degrees per second, ignore vision updates
+      {
+        doRejectUpdate = true;
+      }
+      if(mt2.tagCount == 0)
+      {
+        doRejectUpdate = true;
+      }
+      if(!doRejectUpdate)
+      {
+        PoseEstimator.setVisionMeasurementStdDevs(VecBuilder.fill(.7,.7,9999999));
+        PoseEstimator.addVisionMeasurement(
+            mt2.pose,
+            mt2.timestampSeconds);
+      }
+
     private static Limelight instance;
     
     public static Limelight getInstance(){
---
+++ src/main/java/org/team498/C2024/subsystems/LimelightLocalization.java
@@ -0,0 +1,26 @@
+package org.team498.C2024.subsystems;
+import org.team498.lib.LimelightHelpers;
+import org.team498.lib.LimelightHelpers.PoseEstimate;
+
+import edu.wpi.first.math.VecBuilder;
+
+
+public class LimelightLocalization {
+  LimelightHelpers.SetRobotOrientation("limelight", m_poseEstimator.getEstimatedPosition().getRotation().getDegrees(), 0, 0, 0, 0, 0);
+      LimelightHelpers.PoseEstimate mt2 = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("limelight");
+      if(Math.abs(m_gyro.getRate()) > 720) // if our angular velocity is greater than 720 degrees per second, ignore vision updates
+      {
+        doRejectUpdate = true;
+      }
+      if(mt2.tagCount == 0)
+      {
+        doRejectUpdate = true;
+      }
+      if(!doRejectUpdate)
+      {
+        PoseEstimate.setVisionMeasurementStdDevs(VecBuilder.fill(.7,.7,9999999));
+        m_poseEstimator.addVisionMeasurement(
+            mt2.pose,
+            mt2.timestampSeconds);
+      }
+}
